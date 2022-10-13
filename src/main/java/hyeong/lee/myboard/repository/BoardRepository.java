e51ce75a20022cecb0ae1dc21429edd0a92dbe95
eheh12321 <eheh12321@gmail.com>
2022-10-13 13:14:52 +0900

    Revert "게시글 CRUD 구현 (#2)" (#3)
    This reverts commit 36236c6af1aac18c77815c0acd237fe0fd7bb275.

--- src/main/java/hyeong/lee/myboard/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package hyeong.lee.myboard.repository;
-
-import hyeong.lee.myboard.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
