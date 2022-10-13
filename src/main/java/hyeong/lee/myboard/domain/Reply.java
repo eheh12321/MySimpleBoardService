b85cd948c27c85483943846ee0c32328f39c9b1b
eheh12321 <eheh12321@gmail.com>
2022-10-14 00:28:05 +0900

    feat: 댓글(Reply) 엔티티 추가 및 게시글과 연관관계 매핑 (#12)
    - 게시글(Board, 1) <---> 댓글(Reply,N)
    - 양방향 매핑으로 연결. List를 통해 게시글에 속한 댓글 조회
    - 게시글이 삭제되면 그 내부에 속한 댓글도 함께 삭제됨

--- src/main/java/hyeong/lee/myboard/domain/Reply.java
+++ src/main/java/hyeong/lee/myboard/domain/Reply.java
@@ -3,21 +3,16 @@ package hyeong.lee.myboard.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Board extends AuditingFields {
+public class Reply extends AuditingFields {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(length = 100, nullable = false)
-    private String title;
 
     @Column(length = 50, nullable = false)
     private String editor;
@@ -25,11 +20,10 @@ public class Board extends AuditingFields {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
-    private List<Reply> replies = new ArrayList<>();
+    @ManyToOne(optional = false)
+    private Board board;
 
-    public void updateContent(String title, String content) {
-        this.title = title;
+    public void updateReply(String content) {
         this.content = content;
     }
 }
