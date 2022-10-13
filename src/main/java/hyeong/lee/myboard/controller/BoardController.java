83b14d11118fc7da1f4592569035310b40f9393a
eheh12321 <eheh12321@gmail.com>
2022-10-13 10:19:49 +0900

    feat: 게시글 조회 화면 구성 및 디자인 개선 (#1)
    - 부트스트랩 점보트론 이용해서 깔끔하게 개선

--- src/main/java/hyeong/lee/myboard/controller/BoardController.java
+++ src/main/java/hyeong/lee/myboard/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -22,6 +23,19 @@ public class BoardController {
         Page<BoardResponseDto> boards = boardService.findAllBoards(pageable);
         model.addAttribute("boards", boards);
 
-        return "index";
+        return "board/board-list";
+    }
+
+    @GetMapping("/new")
+    public String createBoard() {
+        return "board/board-new";
+    }
+
+    @GetMapping("/{boardId}")
+    public String boardDetail(@PathVariable Long boardId, Model model) {
+        BoardResponseDto dto = boardService.findById(boardId);
+        model.addAttribute("board", dto);
+
+        return "board/board-detail";
     }
 }
--- src/main/java/hyeong/lee/myboard/service/BoardService.java
+++ src/main/java/hyeong/lee/myboard/service/BoardService.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -17,14 +19,22 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public void create(BoardRequestDto dto) {
+    public Long create(BoardRequestDto dto) {
         Board board = dto.toEntity();
-        boardRepository.save(board);
+        Board savedBoard = boardRepository.save(board);
+        return savedBoard.getId();
     }
 
     @Transactional(readOnly = true)
     public Page<BoardResponseDto> findAllBoards(Pageable pageable) {
         return boardRepository.findAll(pageable)
                 .map(BoardResponseDto::from);
+    }
+
+    @Transactional(readOnly = true)
+    public BoardResponseDto findById(Long boardId) {
+        return boardRepository.findById(boardId)
+                .map(BoardResponseDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다 -> " + boardId));
     }
 }
