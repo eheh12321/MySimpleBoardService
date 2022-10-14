d7b57189e66a1654ec23d76ab21d318fa677e2a5
eheh12321 <eheh12321@gmail.com>
2022-10-14 18:12:41 +0900

    feat: 댓글 엔티티에 사용자 계정 정보 추가 (#14)
    - 게시글과 마찬가지로 비회원도 댓글을 작성할 수 있음 (= UserAccount NULL 가능)
    - 또 마찬가지로 로그인 회원의 경우 작성자 정보를 UserAccount 닉네임으로 변환해서 제공

--- src/main/java/hyeong/lee/myboard/dto/ReplyRequestDto.java
+++ src/main/java/hyeong/lee/myboard/dto/ReplyRequestDto.java
@@ -2,21 +2,26 @@ package hyeong.lee.myboard.dto;
 
 import hyeong.lee.myboard.domain.Board;
 import hyeong.lee.myboard.domain.Reply;
+import hyeong.lee.myboard.domain.UserAccount;
 import lombok.*;
 
-@Getter
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReplyRequestDto {
-
     private Long boardId;
     private String editor;
     private String content;
+    private UserAccountDto userAccountDto;
 
     public Reply toEntity(Board board) {
+        UserAccount userAccount = userAccountDto == null ? null : userAccountDto.toEntity();
+
         return Reply.builder()
                 .board(board)
                 .editor(editor)
-                .content(content).build();
+                .content(content)
+                .userAccount(userAccount)
+                .build();
     }
 }
