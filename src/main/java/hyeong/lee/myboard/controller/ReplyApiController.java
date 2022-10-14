d0efb98db6bd1f64cb836adc2fe970e277ab2cfc
eheh12321 <eheh12321@gmail.com>
2022-10-14 18:15:15 +0900

    feat: 댓글 작성/삭제 권한 분리 구현 (#14)
    - 게시글 방식과 같음
      - 비회원 작성 가능
      - 비회원이 쓴 댓글은 제한없이 삭제 가능 상태 (임시)
      - 회원이 쓴 댓글은 로그인 한 상태이면서 + 회원 정보가 일치하는 경우에 삭제 가능

--- src/main/java/hyeong/lee/myboard/controller/ReplyApiController.java
+++ src/main/java/hyeong/lee/myboard/controller/ReplyApiController.java
@@ -1,9 +1,14 @@
 package hyeong.lee.myboard.controller;
 
+import hyeong.lee.myboard.domain.UserAccount;
 import hyeong.lee.myboard.dto.ReplyRequestDto;
+import hyeong.lee.myboard.dto.UserAccountDto;
+import hyeong.lee.myboard.dto.security.BoardPrincipal;
 import hyeong.lee.myboard.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,14 +19,27 @@ public class ReplyApiController {
     private final ReplyService replyService;
 
     @PostMapping
-    public ResponseEntity<Long> create(@RequestBody ReplyRequestDto dto) {
+    public ResponseEntity<Long> create(
+            @AuthenticationPrincipal @Nullable BoardPrincipal boardPrincipal,
+            @RequestBody ReplyRequestDto dto) {
+
+        if(boardPrincipal != null) {
+            UserAccountDto userAccountDto = boardPrincipal.toDto();
+            dto.setEditor(userAccountDto.getUserId());
+            dto.setUserAccountDto(userAccountDto);
+        }
         Long replyId = replyService.create(dto);
         return ResponseEntity.ok(replyId);
     }
 
     @DeleteMapping("/{replyId}")
-    public ResponseEntity<Long> delete(@PathVariable Long replyId) {
-        replyService.delete(replyId);
+    public ResponseEntity<Long> delete(
+            @AuthenticationPrincipal @Nullable BoardPrincipal boardPrincipal,
+            @PathVariable Long replyId) {
+
+        // 비로그인 상태라도 댓글 삭제 가능 (단, 회원이 작성한 글에 대한 삭제는 불가능)
+        UserAccount userAccount = boardPrincipal == null ? null : boardPrincipal.toEntity();
+        replyService.delete(replyId, userAccount);
         return ResponseEntity.ok(replyId);
     }
 }
