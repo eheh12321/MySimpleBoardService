5d7889376091f41384117c80f5a7c060c5201db3
eheh12321 <eheh12321@gmail.com>
2022-10-14 18:41:24 +0900

    feat: 게시글/댓글 작성자 회원/비회원 유무 색으로 구분되도록 개선 (#14)
    - 엔티티의 UserAccount 유무를 이용해 회원/비회원 유무 정보 추가 삽입
    - 로그인 회원의 경우 녹색으로 구분해서 눈에 띄도록 만듬
    
    (나중에 눌러서 사용자 정보 보기 기능도 만들어보려면 애초에 그냥 UserAccountResponseDto도 같이 넘겨버리는것도 괜찮을 것 같은데 일단은 이정도만 하기로 함)

--- src/main/java/hyeong/lee/myboard/dto/ReplyResponseDto.java
+++ src/main/java/hyeong/lee/myboard/dto/ReplyResponseDto.java
@@ -15,11 +15,13 @@ public class ReplyResponseDto {
     private final String content;
     private final LocalDateTime createdAt;
     private final UserAccountResponseDto replyUserAccount;
+    private final boolean isRegisteredUser; // 익명 사용자 유무
 
     public static ReplyResponseDto from(Reply reply) {
         String replyEditorNickname = reply.getUserAccount() == null ? reply.getEditor() : reply.getUserAccount().getNickname();
         UserAccountResponseDto userAccountResponseDto =
                 reply.getUserAccount() == null ? null : UserAccountResponseDto.from(reply.getUserAccount());
+        boolean isRegisteredUser = reply.getUserAccount() != null;
 
         return ReplyResponseDto.builder()
                 .id(reply.getId())
@@ -27,6 +29,7 @@ public class ReplyResponseDto {
                 .content(reply.getContent())
                 .createdAt(reply.getCreatedAt())
                 .replyUserAccount(userAccountResponseDto)
+                .isRegisteredUser(isRegisteredUser)
                 .build();
     }
 }
