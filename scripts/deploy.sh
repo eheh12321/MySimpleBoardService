a3030ad428fb933b195fdc43561a725f124f710b
eheh12321 <eheh12321@gmail.com>
2022-10-17 17:29:24 +0900

    build: rootProject 이름 변경
    - 깃허브 프로젝트 이름이 mySimpleBoardService 인데 빌드 되는 프로젝트 이름은 myboard임
    - Settings.gradle 안에 rootProject.name 설정 바꿔서 통일성 있게 맞춤

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=mySimpleBoardService
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl mySimpleBoardService | grep jar | awk '{pring $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{pring $1}')
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
