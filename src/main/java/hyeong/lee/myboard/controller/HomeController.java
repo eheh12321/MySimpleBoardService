625d96d8d8eac792ec409f31eeb294bfc01d5002
eheh12321 <eheh12321@gmail.com>
2022-10-21 13:51:40 +0900

    feat: 관리자만 접근 가능한 페이지 추가 (#28)
    - SecurityConfig에서 hasRole() 사용시 "ROLE_" 접두사는 떼고 사용해야 한다 (시큐리티가 자동으로 ROLE_을 붙여서 사용한다)
    -> ROLE_ADMIN으로 적어놓으면 ROLE_ROLE_ADMIN이 되어버려 권한 설정 장애 발생
    - hasAuthority() 메서드 사용 시 ROLE_ADMIN 적어도 됨

--- src/main/java/hyeong/lee/myboard/controller/HomeController.java
+++ src/main/java/hyeong/lee/myboard/controller/HomeController.java
@@ -1,9 +1,13 @@
 package hyeong.lee.myboard.controller;
 
+import hyeong.lee.myboard.dto.security.BoardPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @RequestMapping("/")
 @Controller
 public class HomeController {
@@ -11,5 +15,10 @@ public class HomeController {
     @GetMapping
     public String forward() {
         return "forward:/boards";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin/index";
     }
 }
