9268df75483dfca343f4e703d665e36d211a89b3
eheh12321 <eheh12321@gmail.com>
2022-10-22 01:38:22 +0900

    feat: 로그인 성공 이후 이전에 보고있던 페이지로 리다이렉트 되도록 개선 (#30)
    - HTTP Request 중 referer 헤더와 세션 사용
    - 세션에 "url_prior_login" 정보를 담아놓고 로그인에 성공하면 해당 값을 사용해 리다이렉트를 보내고, 세션에 저장된 정보는 삭제하는 방식.
    
    * https://stackoverflow.com/questions/14573654/spring-security-redirect-to-previous-page-after-successful-login
    * https://www.baeldung.com/spring-security-redirect-login

--- src/main/java/hyeong/lee/myboard/controller/HomeController.java
+++ src/main/java/hyeong/lee/myboard/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RequestMapping("/")
 @Controller
@@ -25,11 +27,18 @@ public class HomeController {
     // @PostMapping("/login")은 스프링 시큐리티가 내부적으로 처리한다
     @GetMapping("/login")
     public String loginPage(@ModelAttribute("error") String loginFailureError,
+                            HttpServletRequest request,
                             Model model) {
+
         // flashMap을 통해 loginFailureError 값이 전달 된 경우 (없을 경우 ""이 리턴되기 때문에 isEmpty() 처리)
         // ** flashMap 특성 상 리다이렉트되는 시점에 1회만 꺼낼 수 있음 (새로고침시 더이상 보이지 않음) **
         if(loginFailureError != null && !loginFailureError.isEmpty()) {
             model.addAttribute("loginFailureError", loginFailureError);
+        } else {
+            // 로그인 페이지에 새로 들어온 경우에만 <-- 로그인 실패로 리다이렉트된 경우 referer 값을 덮어씌워버리기 때문
+            // 세션에 이전 페이지 경로 정보를 referer 헤더를 통해 가져온 뒤 저장해둔다
+            String referer = request.getHeader("referer");
+            request.getSession().setAttribute("url_prior_login", referer);
         }
         return "/login";
     }
--- src/main/java/hyeong/lee/myboard/handler/LoginSuccessHandler.java
+++ src/main/java/hyeong/lee/myboard/handler/LoginSuccessHandler.java
@@ -7,26 +7,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private final UserAccountRepository userAccountRepository;
-
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
-    }
 
     @Transactional // UserAccount 테이블 update가 필요하므로 필요
     @Override
@@ -35,6 +31,16 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
         // 로그인에 성공했으므로 로그인 시도횟수 0으로 초기화
         userAccountRepository.findById(principal.getUsername())
                         .ifPresent(UserAccount::loginSuccess);
+
+        HttpSession session = request.getSession();
+        if(session != null) {
+            String redirectUrl = (String) session.getAttribute("url_prior_login");
+            if(redirectUrl != null) {
+                session.removeAttribute("url_prior_login");
+                getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+                return;
+            }
+        }
         response.sendRedirect("/");
     }
 }
