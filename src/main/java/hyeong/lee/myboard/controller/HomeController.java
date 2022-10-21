d59edfa000e30438fa4312c496ed4ae947bcd452
eheh12321 <eheh12321@gmail.com>
2022-10-21 23:04:35 +0900

    feat: 로그인 실패 시 flashMap을 이용한 에러문구 전달 (#30)
    - flashMap을 이용해 리다이렉트 시 1회에 한해 값을 같이 전달해줄 수 있음
    - 리다이렉트 되고 난 이후 새로고침 하거나 이동하고 나면 더이상 값을 얻을 수 없음
    - PRG(Post-Redirect-Get) 패턴에서 유용하게 쓰인다
    - 값을 쿼리 파라미터로 붙여서 보내지 않고 Map 구조에 담아서 보내준다
    -> URL에 데이터를 노출시키지 않으면서도 세션 데이터에 넣어서 사용하기에는 부담되는 경우에 적합
    - 세션과 같은 장소에 값을 저장해뒀다가 리다이렉트 후 즉시 삭제하는 방식으로 동작. 휘발성
    
    * 원본 코드: https://stackoverflow.com/questions/23844546/flash-attribute-in-custom-authenticationfailurehandler/50429613#50429613
    
    * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/FlashMap.html
    * https://nbkw.tistory.com/135
    * https://www.baeldung.com/spring-web-flash-attributes

--- src/main/java/hyeong/lee/myboard/controller/HomeController.java
+++ src/main/java/hyeong/lee/myboard/controller/HomeController.java
@@ -1,10 +1,10 @@
 package hyeong.lee.myboard.controller;
 
-import hyeong.lee.myboard.dto.security.BoardPrincipal;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -24,7 +24,13 @@ public class HomeController {
 
     // @PostMapping("/login")은 스프링 시큐리티가 내부적으로 처리한다
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(@ModelAttribute("error") String loginFailureError,
+                            Model model) {
+        // flashMap을 통해 loginFailureError 값이 전달 된 경우 (없을 경우 ""이 리턴되기 때문에 isEmpty() 처리)
+        // ** flashMap 특성 상 리다이렉트되는 시점에 1회만 꺼낼 수 있음 (새로고침시 더이상 보이지 않음) **
+        if(loginFailureError != null && !loginFailureError.isEmpty()) {
+            model.addAttribute("loginFailureError", loginFailureError);
+        }
         return "/login";
     }
 }
--- src/main/java/hyeong/lee/myboard/handler/LoginFailureHandler.java
+++ src/main/java/hyeong/lee/myboard/handler/LoginFailureHandler.java
@@ -3,8 +3,10 @@ package hyeong.lee.myboard.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.FlashMapManager;
+import org.springframework.web.servlet.support.SessionFlashMapManager;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,8 +15,14 @@ import java.io.IOException;
 public class LoginFailureHandler implements AuthenticationFailureHandler {
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        log.info(">> 로그인 실패!");
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        if(exception != null) {
+            final FlashMap flashMap = new FlashMap();
+            flashMap.put("error", "아이디 혹은 비밀번호를 확인해주세요");
+
+            final FlashMapManager flashMapManager = new SessionFlashMapManager();
+            flashMapManager.saveOutputFlashMap(flashMap, request, response);
+        }
         response.sendRedirect("/login");
     }
 }
