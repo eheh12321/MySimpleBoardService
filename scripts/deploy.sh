bedcbf7848652092b7c1d4af22aeb63f24f938f4
eheh12321 <eheh12321@gmail.com>
2023-02-15 18:39:40 +0900

    build: prod 환경 설정 파일 분리 (RDS 연결)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/mySimpleBoardService
+REPOSITORY=/home/ec2-user/app/mySimpleBoardService/zip/build/libs
 PROJECT_NAME=mySimpleBoardService
 
 echo "> 현재 8080 포트를 사용중인 애플리케이션 종료"
-
-sudo kill $(lsof -t -i:8080)
-
-echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
-
-cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
+sudo kill -15 $(lsof -t -i:8080)
 
 echo "> 새 애플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행 권한 추가"
-
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 java -jar \
         -Dspring.config.location=classpath:/application.yml \
         -Dspring.profiles.active=prod \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
